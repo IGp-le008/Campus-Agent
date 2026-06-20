4252fc9f0f34b2c8f612216e59043cdbd33844f6
Piyush Jha <jhapeeyush1@gmail.com>
2026-06-20 23:15:08 +0545

    Edited 4_pydantic_parser_chain.py

--- 2_chains/app/4_pydantic_parser_chain.py
+++ 2_chains/app/4_pydantic_parser_chain.py
@@ -1,6 +1,6 @@
 import os
 from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
