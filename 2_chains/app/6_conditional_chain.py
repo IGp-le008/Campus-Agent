d94c03c678d2292ca090d39ce5768012b3c9b3ff
Piyush Jha <jhapeeyush1@gmail.com>
2026-06-21 17:20:02 +0545

    Fixed the missing {fornat_instructions}

--- 2_chains/app/6_conditional_chain.py
+++ 2_chains/app/6_conditional_chain.py
@@ -24,7 +24,7 @@ class FeedBack(BaseModel):
 pydantic_parser = PydanticOutputParser(pydantic_object=FeedBack)
 
 prompt_sentiment=PromptTemplate(
-    template='Classify the sentiment of the following feedback from user as either positive or negative: {feedback}',
+    template='Classify the sentiment of the following feedback from user as either positive or negative: {feedback} in the format: {format_instructions}',
     input_variables=['feedback'],
     partial_variables={'format_instructions': pydantic_parser.get_format_instructions()}
 )
@@ -51,15 +51,19 @@ branch_chain=RunnableBranch(
 
 
 classifier_chain = prompt_sentiment | model | pydantic_parser
-final_chain = classifier_chain | branch_chain
 
-result=final_chain.invoke({'feedback': 'I love the new features in your product! It has made my life so much easier.'})
-
-print("-----------------------------------------ConditionalChainResult-----------------------------------------\n")
-print(result)
+print(classifier_chain.invoke({'feedback':'I like this product!'}))
 
 
-print("\n-----------------------------------------Execution Graph---------------------------------------\n")
-final_chain.get_graph().print_ascii()
+# final_chain = classifier_chain | branch_chaincler
+
+# result=final_chain.invoke({'feedback': 'I love the new features in your product! It has made my life so much easier.'})
+
+# print("-----------------------------------------ConditionalChainResult-----------------------------------------\n")
+# print(result)
+
+
+# print("\n-----------------------------------------Execution Graph---------------------------------------\n")
+# final_chain.get_graph().print_ascii()
 
 
