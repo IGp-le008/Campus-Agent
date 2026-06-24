4a2fac926c4c44405ce790a67a535495666a65bb
Piyush Jha <jhapeeyush1@gmail.com>
2026-06-24 15:51:25 +0545

    Modified! Working Passthrough

---
+++ 2_chains/app/9_runnable_passthrough.py
@@ -0,0 +1,46 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+model = ChatOpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    model="Qwen/Qwen2.5-7B-Instruct"
+)
+
+prompt1 = PromptTemplate(
+    template="Write 3 points about the topic: {topic}",
+    input_variables=["topic"]
+)
+
+prompt2=PromptTemplate(
+    template="Write a summary of the following points: {points}",
+    input_variables=["points"]
+)
+
+parser=StrOutputParser()
+
+point_chain= RunnableSequence(prompt1,model,parser)
+
+parallel_chain= RunnableParallel({
+    "points":RunnablePassthrough(),
+    "summary":RunnableSequence(prompt2,model,parser)
+})
+
+final_chain= RunnableSequence(point_chain,parallel_chain)
+
+result=final_chain.invoke({"topic":"Artificial Intelligence"})
+
+print("------------------------------Runnable Passthrough Result---------------------------------")
+
+print(result)
+
+
+print("------------------------------Execution Graph---------------------------------  ")
+final_chain.get_graph().print_ascii()
